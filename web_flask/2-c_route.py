31ecaf141f63e8291bfdae44d81169701ca256b0
Madani00 <mamchart@gmail.com>
2024-05-18 14:12:45 -0700

    well DONE

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def home():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<changeble_text>')
+def c_is_c(changeble_text):
+    text = changeble_text.replace("_", " ")
+    return "C " + text 
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
