e93bad937bb40e0625f151688443640f2fe35ab8
Madani00 <mamchart@gmail.com>
2024-05-18 17:09:10 -0700

    magic

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def home():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<changeble_text>')
+def c_is_c(changeble_text):
+    text = changeble_text.replace("_", " ")
+    return "C " + text
+
+
+@app.route('/python/<changeble_text>', defaults={'changeble_text': 'is cool'})
+def py(changeble_text):
+    text = changeble_text.replace("_", " ")
+    return "Python " + text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
